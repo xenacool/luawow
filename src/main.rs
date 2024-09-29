eb204c62b74ec4cfebc89a60324f4d62b103fd4b
xenacoolz <small.sock79281@fastmail.com>
2024-09-28 17:28:32 -0700

    zone manifest

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use bevy::prelude::{App, DefaultPlugins, Reflect};
 use bevy_editor_pls::{AddEditorWindow, EditorPlugin};
 use bevy_save::SavePlugins;
-use crate::zone_editor::ZoneEditorWindow;
+use crate::zone_editor::{TileData, TransformZoneManifest, ZoneCollectionManifest, ZoneEditorWindow, ZoneManifest};
 
 mod zone_editor;
 
-#[derive(Reflect)]
-pub struct ZoneCollection;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, EditorPlugin::new(), SavePlugins))
-        .register_type::<ZoneCollection>()
+        .register_type::<TileData>()
+        .register_type::<ZoneManifest>()
+        .register_type::<TransformZoneManifest>()
+        .register_type::<ZoneCollectionManifest>()
         .add_editor_window::<ZoneEditorWindow>()
         .run();
 }
